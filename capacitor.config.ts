4319359e10ef89b55b8184d36860e82b2886ff67
Cédric Meyer <mecedricod@gmail.com>
2021-11-29 17:01:55 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-vue-compar-izon',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
